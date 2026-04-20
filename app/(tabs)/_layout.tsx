9302cc3685592dee4078028a18afea6a1ad2e6ea
Lorenzo <lorenzzo.acquesta@gmail.com>
2026-04-20 18:26:29 -0300

    Feat: Estilizando tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,76 @@
-import { Tabs } from 'expo-router';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome6 as FontAwesome } from '@expo/vector-icons';
+import { TabList, Tabs, TabSlot, TabTrigger, TabTriggerSlotProps } from 'expo-router/ui';
+import { forwardRef } from 'react';
+import { Pressable, Text, View } from 'react-native';
 import { Colors } from '../../constants/Constants';
+
+type Icon = React.ComponentProps<typeof FontAwesome>['name'];
+
+type TabButtonProps = TabTriggerSlotProps & {
+  icon: Icon;
+  label: string;
+};
+
+const TabButton = forwardRef<View, TabButtonProps>(
+  ({ icon, label, isFocused, ...props }, ref) => {
+    return (
+      <Pressable
+        ref={ref}
+        {...props}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FontAwesome
+          name={icon}
+          size={24}
+          color={isFocused ? Colors.azul : "#C0C0C2"}
+        />
+        <Text style={{
+          fontSize: 12,
+          color: isFocused ? Colors.azul : "#C0C0C2"
+        }}>
+          {label}
+        </Text>
+      </Pressable>
+    );
+  }
+);
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: Colors.azul,
-        tabBarStyle: {
-          backgroundColor: Colors.light.background,
-        }
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Início',
-          tabBarIcon: ({ color }) => <FontAwesome size={24} name="home" color={color} />,
+    <Tabs>
+      <TabSlot />
+
+      <TabList
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          flexDirection: 'row',
+          backgroundColor: Colors.light.branco_cards,
+          margin: 23,
+          borderRadius: 16,
+          height: 65,
+          borderColor: Colors.cinza,
+          borderWidth: 1,
         }}
-      />
-      <Tabs.Screen
-        name="agendamento"
-        options={{
-          title: 'Agendamento',
-          tabBarIcon: ({ color }) => <FontAwesome size={24} name="calendar" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: 'ChatIA',
-          tabBarIcon: ({ color }) => <FontAwesome size={24} name="comments" color={color} />,
-        }}
-      />
+      >
+        <TabTrigger name="index" href="/(tabs)" asChild>
+          <TabButton icon="house" label="Início" />
+        </TabTrigger>
+
+        <TabTrigger name="agendamento" href="/(tabs)/agendamento" asChild>
+          <TabButton icon="calendar-days" label="Agendamento" />
+        </TabTrigger>
+
+        <TabTrigger name="chat" href="/(tabs)/chat" asChild>
+          <TabButton icon="comments" label="Chat IA" />
+        </TabTrigger>
+      </TabList>
     </Tabs>
   );
 }
