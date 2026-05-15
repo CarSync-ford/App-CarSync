d9cf4bd8efc6df9cadccd82708b07a12ca6d6e33
Lorenzo <lorenzzo.acquesta@gmail.com>
2026-05-15 15:13:24 -0300

    feat: cria rota de cadastro no expo router

---
+++ app/register.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import RegisterContainer from '@/src/components/register/RegisterContainer';
+
+export default function RegisterScreen() {
+  return <RegisterContainer />;
+}
